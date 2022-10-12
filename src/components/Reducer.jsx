c63868ddd6ac7e94fa863d6d7a0405b135d3d584
ksagar03 <sagar22krishna03@gmail.com>
2022-10-12 21:06:48 +0530

    working on react-context-api

---
+++ src/components/Reducer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// this file is created for manupilating the data layer
+
+export const initialstate = {
+  Basket: [],
+};
+
+//This below code defines the how or when to dispatch a data to data layer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        Basket: [...state.Basket, action.item],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './components/StateProvider';
+import reducer, { initialstate} from './components/Reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StateProvider initialState={initialstate} reducer={reducer}>
     <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
