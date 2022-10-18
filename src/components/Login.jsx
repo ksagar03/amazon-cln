1ccaf644145867169411fee1a7f82252aa1f8f70
ksagar03 <sagar22krishna03@gmail.com>
2022-10-18 12:05:25 +0530

    completed login page styleing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Login.css";
 import { Link } from "react-router-dom";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIN = (event) => {
+    event.preventDefault();
+  };
+  const registerAccount = (event) => {
+    event.preventDefault(); // this line will prevent from reloading the page whenever we click on the btn 
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -15,19 +23,32 @@ const Login = () => {
       <div className="login__container">
         <h1>Sign-in</h1>
         <form>
-            <h5><strong>E-mail</strong></h5>
-            <input type="text" /> 
-            <h5><strong>Password</strong></h5>
-            <input type="password" />
-            <button>Sign in</button>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={signIN} className="login__signInBtn">
+            Sign in
+          </button>
         </form>
         <p>
-        By continuing, you agree to Amazon's clone Conditions of Use and Privacy Notice.
+          By continuing, you agree to Amazon's clone Conditions of Use and
+          Privacy Notice.
         </p>
         <h6>New to amazon?</h6>
-        <button>Create your Amazon account</button>
+        <button onclick={registerAccount} className="login__newRegistrationBtn">
+          Create your Amazon Account
+        </button>
       </div>
-    </div> 
+    </div>
   );
 };
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyALHEhDiWj2JCbBNXn8Rxh8jAj8I-YVN88",
-    authDomain: "clone-6f9dc.firebaseapp.com",
-    projectId: "clone-6f9dc",
-    storageBucket: "clone-6f9dc.appspot.com",
-    messagingSenderId: "121377179512",
-    appId: "1:121377179512:web:bba62904ec5e19fac3aa8d",
-    measurementId: "G-QD10XFEVE6"
-  };
+  apiKey: "AIzaSyALHEhDiWj2JCbBNXn8Rxh8jAj8I-YVN88",
+  authDomain: "clone-6f9dc.firebaseapp.com",
+  projectId: "clone-6f9dc",
+  storageBucket: "clone-6f9dc.appspot.com",
+  messagingSenderId: "121377179512",
+  appId: "1:121377179512:web:bba62904ec5e19fac3aa8d",
+  measurementId: "G-QD10XFEVE6",
+};
