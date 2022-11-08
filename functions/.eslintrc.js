0d8d8df6fc17521e5d67ed0cce1a369c4fb896c7
ksagar03 <sagar22krishna03@gmail.com>
2022-11-08 21:39:59 +0530

    solved Eslint issue in functions-> index.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   root: true,
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
-    quotes: ["error", "double"],
+    "max-len": [
+      "error",
+      {
+        "code": 60,
+        "tabWidth": 2,
+        "ignoreComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+      },
+    ],
+    "quotes": ["error", "double"],
   },
 };
--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,8 @@ const functions = require("firebase-functions");
 
 const express = require("express");
 const cors = require("cors");
-const { response, request } = require("express");
-const stripe = require("stripe")(
-  "sk_test_51Lx20gSHgvSf9YWJE38GQ3cVmP0AGMzoN5DDV9HKWkmXTdNUZg3sYGc2awYKggWJ70cjpKYmXMJjjvBXn9r4nj1Z009XYH8Yiw"
-);
+const {response} = require("express");
+const stripe = require("stripe")("sk_test_51Lx20gSHgvSf9YWJE38GQ3cVmP0AGMzoN5DDV9HKWkmXTdNUZg3sYGc2awYKggWJ70cjpKYmXMJjjvBXn9r4nj1Z009XYH8Yiw");
 
 // to setup an API we need to have fallowing code
 /* 1. API config
@@ -17,26 +15,30 @@ const stripe = require("stripe")(
 const app = express();
 //  2. API middleware
 
-app.use(cors({ origin: true })); // this is like a security which will allow the some perticular
+app.use(cors({origin: true}));
+// this is like a security which will allow the some perticular
 // hosts to access this code
 app.use(express.json());
 
 // 3. API Routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
-app.post("/payments/create", async (request, response) => {
+app.post("/payments/create", async (request) => {
   const total = request.query.total;
   // here /payment/create is a path defined inside payment.js
-  // (/payments/create?total=${to_get_final_subtotal(Basket) * 100}`)  here ?total is called query prams which allows
+  // (/payments/create?total=${to_get_final_subtotal(Basket) * 100}`)
+  // here ?total is called query prams which allows
   // user to define query or logical operation within the url
   // and this post method will send a post request to the
-  // defined path and  returns with a secret key required for the payment processing
-  // this secret key will change when ever there is a change in subtotal(i.e total).
+  // defined path and  returns with a secret key required
+  // for the payment processing
+  // this secret key will change when ever there is
+  // a change in subtotal(i.e total).
 
-  console.log(
-    "payment request has been made and the final total amt ==>",
-    total
-  );
+  // console.log(
+  //   "payment request has been made and the final total amt ==>",
+  //   total
+  // );
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total,
@@ -49,9 +51,14 @@ app.post("/payments/create", async (request, response) => {
 
 // 4. Listen command
 exports.api = functions.https.onRequest(app);
+// to deploay this backend to the firebase:
+/*     1. we need to be function directory
+       2. firebase deploy --only functions */
 
-//  to check whether this backend is working or not, we can emulate this this firbase
-// (note: still we have not deployed this to firebase we are just checking whether it is working or not)
+//  to check whether this backend is
+//  working or not, we can emulate this this firbase
+// (note: still we have not deployed
+// this to firebase we are just checking whether it is working or not)
 //  Command: firebase emulators:start
 //  this command will create a fake server(http://127.0.0.1:5001/clone-6f9dc/us-central1/api) which will boot up the backend
 
